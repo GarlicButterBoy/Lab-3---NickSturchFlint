05885174fc6fef2f5af948308413b03b8cb77eb6
Nick Sturch-Flint <nick.rulez@live.com>
2020-11-14 11:48:19 -0500

    Added a main class, Share

---
+++ Lab 3 - NickSturchFlint/Share.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3___NickSturchFlint
+{
+    class Share
+    {
+        //Data Members
+        protected string buyerName;
+        protected string buyDate;
+        protected double shares;
+        //Getter and Setter for Name
+        public string Name
+        {
+            get { return this.buyerName; }
+            set { this.buyerName = value; }
+        }
+        //Getter and Setter for Date
+        public string Date
+        {
+            get { return this.buyDate; }
+            set { this.buyDate = value; }
+        }
+        //Getter and Setter for Shares
+        public double Shares
+        {
+            get { return this.shares; }
+            set { this.shares = value; }
+        }
+
+        //Constructor
+        public Share(string name, string date, double shares)
+        {
+            this.buyerName = name;
+            this.buyDate = date;
+            this.shares = shares;
+        }
+        //Virtual Method
+        public virtual double ShareValue()
+        {
+            double value = this.shares * 42;
+            return value;
+        }
+    }
+}
